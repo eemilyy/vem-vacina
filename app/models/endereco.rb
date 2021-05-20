20c22abffc41257b424716cf382334167b158ae5
Emily <emilyalmeeidas@gmail.com>
2021-05-19 21:47:10 -0300

    Apagando e em seguida inserindo somente o model de Endereco

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,9 +1,3 @@
 class Endereco < ApplicationRecord
   belongs_to :usuario
-
-  validates_numericality_of :cep, length: {minimum:7}
-  validates :cidade, length: {minimum:3}
-  validates :bairro, length: {minimum:3}
-  validates :logradouro, length: {minimum:5}
-
 end
